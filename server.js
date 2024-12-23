22ca23c237cecd68fa854a420d61ba8bece37282
shub-10 <116427176+shub-10@users.noreply.github.com>
2024-12-23 13:18:17 +0530

    new data added

--- server.js
+++ server.js
@@ -1,6 +1,25 @@
+
+require('dotenv').config()
 const express = require("express")
 const app = express();
 
-app.listen(3000, ()=>{
-  console.log("Ha Ji");
+
+app.get('/shubham', (req, res)=>{
+   res.send("<h1>shubham is 929 at codeforces</h1>")
+})
+
+app.get('/yashavi', (req, res)=>{
+  res.send("ke haal he");
+})
+
+app.get('/abhinav', (req, res)=>{
+  res.send("hello 2025")
+})
+app.get('/bca', (req, res)=>{
+  res.send("tu bhar nikal madarchod")
+})
+
+app.listen(
+  process.env.PORT, ()=>{
+  console.log("Ha Ji")
 })
